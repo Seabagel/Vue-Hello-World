510b7b791464719e4e0269ce6df6c3cdff2b730b
Seabagel <hsrt1367tim@gmail.com>
2021-11-18 21:27:03 -0800

    Basic Vue methods

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ console.log("Hello Vue");
 const app = Vue.createApp({
   //  template: "<h2>I'm a template</h2>"
   data() {
-    return {};
+    return {
+      show: true,
+      messages: [
+        { message: "I have diahrrea", author: "Pudding" },
+        { message: "Real trap shit", author: "YoungJelly" },
+      ],
+    };
+  },
+  methods: {
+    changeMessage(message) {
+      this.messages[0].message = message;
+    },
+    toggleShow() {
+      this.show = !this.show;
+    },
   },
 });
 
